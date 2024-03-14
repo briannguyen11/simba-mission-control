966380b5a65d7be00dfb47dc717f801a2b5023c8
Christian Honein <61818445+chonein@users.noreply.github.com>
2024-03-14 15:58:54 -0700

    added gamepad code
    Co-authored-by: Brian Nguyen <briannguyen11@users.noreply.github.com>

--- react-frontend/src/components/UserInput.js
+++ react-frontend/src/components/UserInput.js
@@ -1,6 +1,6 @@
 // UserInput.js
 
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import axios from "axios";
 import { Typography, Button, TextField, Grid, Box } from "@mui/material";
 
@@ -35,6 +35,8 @@ export default function UserInput({ updateLog, setRouteData, isConnected }) {
   const [motorDist, setMotorDist] = useState("");
   const [pickupDone, setPickupDone] = useState(true);
   const [inProgCount, setInProgCount] = useState(0);
+  const [gamepad, setGamepad] = useState(null);
+  const [prevButtonStates, setPrevButtonStates] = useState([]);
   const armPickupInterval = useRef(null);
 
   const isNumber = (value) => !isNaN(parseFloat(value)) && isFinite(value);
@@ -118,7 +120,32 @@ export default function UserInput({ updateLog, setRouteData, isConnected }) {
   //   }
   // };
 
-  const sendStartArm = async () => {
+  const checkPickupStatus = useCallback(async () => {
+    try {
+      const response = await axios.get("/api/pickup-status");
+      if (response.data.message === "True") {
+        updateLog("Pickup sequence: Completed");
+        setPickupDone(true);
+        setInProgCount(0);
+        // stop further interval checks if the sequence is completed
+        clearInterval(armPickupInterval.current);
+      } else {
+        // arm pickup sequence is still in progress
+        setInProgCount((prevCount) => {
+          console.log(prevCount); // debug ... just printing 0
+          const newCount = prevCount + 1;
+          updateLog(`Pickup sequence: In Progress [${newCount}]`);
+          return newCount; // Return the updated value
+        });
+      }
+    } catch (error) {
+      console.error("Error checking arm pickup status:", error);
+    }
+  }, [updateLog]);
+
+  // it's ok i will keep this code separate  not sure if it will work
+  // thanks
+  const sendStartArm = useCallback(async () => {
     if (isConnected) {
       try {
         const response = await axios.post("/api/start-pickup");
@@ -135,30 +162,97 @@ export default function UserInput({ updateLog, setRouteData, isConnected }) {
     } else {
       alert("Rover already disconnected.");
     }
-  };
+  }, [checkPickupStatus, isConnected, updateLog]);
 
-  const checkPickupStatus = async () => {
-    try {
-      const response = await axios.get("/api/pickup-status");
-      if (response.data.message === "True") {
-        updateLog("Pickup sequence: Completed");
-        setPickupDone(true);
-        setInProgCount(0);
-        // stop further interval checks if the sequence is completed
-        clearInterval(armPickupInterval.current);
-      } else {
-        // arm pickup sequence is still in progress
-        console.log(inProgCount); // debug ... just printing 0
-        setInProgCount((prevCount) => {
-          const newCount = prevCount + 1;
-          updateLog(`Pickup sequence: In Progress [${newCount}]`);
-          return newCount; // Return the updated value
-        });
-      }
-    } catch (error) {
-      console.error("Error checking arm pickup status:", error);
-    }
-  };
+  useEffect(() => {
+    const handleGamepadConnection = (event) => {
+      const gamepad = event.gamepad;
+      setGamepad(gamepad);
+      console.log("Gamepad connected:", gamepad);
+    };
+
+    // Function to handle gamepad disconnection
+    const handleGamepadDisconnection = (event) => {
+      setGamepad(null);
+      console.log("Gamepad disconnected:", event.gamepad);
+    };
+    window.addEventListener("gamepadconnected", handleGamepadConnection);
+    window.addEventListener("gamepaddisconnected", handleGamepadDisconnection);
+
+    return () => {
+      window.removeEventListener("gamepadconnected", handleGamepadConnection);
+      window.removeEventListener(
+        "gamepaddisconnected",
+        handleGamepadDisconnection
+      );
+    };
+  }, []);
+
+  useEffect(() => {
+    // Function to check gamepad input
+    const checkGamepadInput = async () => {
+      if (!gamepad) return;
+
+      // Check D-PAD button presses
+      const dPadButtons = {
+        12: "ArrowUp",
+        13: "ArrowDown",
+        14: "ArrowLeft",
+        15: "ArrowRight",
+      };
+
+      // Check A, X, B, Y button presses
+      const actionButtons = {
+        0: "A",
+        2: "X",
+        1: "B",
+        3: "Y",
+      };
+
+      // Iterate through buttons and check their states
+      gamepad.buttons.forEach((button, index) => {
+        const prevButtonState = prevButtonStates[index];
+        if (button.pressed && !prevButtonState) {
+          // Check D-PAD buttons
+          if (dPadButtons[index] || actionButtons[index] === "A") {
+            const direction =
+              actionButtons[index] === "A" ? "Stop" : dPadButtons[index];
+            if (isConnected) {
+              axios
+                .post("/api/arrow-keys", {
+                  direction: direction,
+                })
+                .then((response) => {
+                  updateLog(response.data.message);
+                })
+                .catch(() => {
+                  alert("Rover not connected.");
+                });
+            }
+            console.log("D-PAD Button Pressed:", direction);
+          }
+
+          // Check action buttons
+          if (actionButtons[index] === "Y") {
+            // do arm stuff
+            sendStartArm();
+            console.log("Action Button Pressed:", actionButtons[index]);
+          }
+        }
+      });
+    };
+
+    const gameLoop = () => {
+      checkGamepadInput();
+      setPrevButtonStates(
+        gamepad ? gamepad.buttons.map((button) => button.pressed) : []
+      );
+      requestAnimationFrame(gameLoop);
+    };
+
+    const animationFrameId = requestAnimationFrame(gameLoop);
+    return () => cancelAnimationFrame(animationFrameId);
+  }, [gamepad, prevButtonStates, isConnected, updateLog, sendStartArm]);
 
   return (
     <Box
